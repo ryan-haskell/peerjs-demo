4a1ae2edc30c6c56e369a65644770de6ccbe4152
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-07-11 23:07:12 -0500

    latency resolved?

--- src/main.js
+++ src/main.js
@@ -4,9 +4,24 @@ const config = {
   reliable: true
 }
 
+let localQueue = []
+let otherQueue = []
+
+const perform = (msg) => localQueue.push(msg)
+const streamActions = (conn) =>
+  setInterval(_ => conn.send(localQueue), 100)
+
+const onOpen = ({ id, conn, onData }) => {
+  console.log('CONNECTED')
+  streamActions(conn)
+  // Receive messages
+  conn.on('data', msg => console.log('onData', msg) || onData(msg))
+  // Send messages
+  perform({ action: 'FRIEND_READY', payload: String(id) })
+}
 
 const rtc = {
-  host: ({ id, onData, onConnect }) =>
+  host: ({ id, onData }) =>
     new Promise((resolve, reject) => {
       const peer = new window.Peer(id, config)
       peer.on('open', function (id) {
@@ -18,28 +33,14 @@ const rtc = {
         reject(err)
       })
       peer.on('connection', function (conn) {
-        conn.on('open', function () {
-          onConnect(conn)
-          // Receive messages
-          conn.on('data', onData)
-          // Send messages
-          conn.send({ action: 'FRIEND_READY', payload: String(id) })
-        })
+        conn.on('open', onOpen({ id, conn, onData }))
       })
   }),
-  join: ({ id, serverId, onData, onConnect }) => {
+  join: ({ id, serverId, onData }) => {
     const peer = new window.Peer(id, config)
     const conn = peer.connect(serverId)
-    conn.on('open', function () {
-      onConnect(conn)
-      // Receive messages
-      conn.on('data', onData)
-      // Send messages
-      conn.send({ action: 'FRIEND_READY', payload: String(id) })
-    })
+    conn.on('open', onOpen({ id, conn, onData }))
   },
-  send: ({ conn, msg }) =>
-    conn && conn.send(msg)
 }
 
 
@@ -53,23 +54,27 @@ const start = () => {
     }
   })
 
-  let shared = { conn: null }
-
-  const onData = (msg) => {
-    console.log('INCOMING', msg)
-    app.ports.incoming.send(msg)
+  const onData = (latestOtherQueue) => {
+    console.log(latestOtherQueue)
+    const changes = latestOtherQueue.slice(otherQueue.length)
+    if (changes.length > 0) {
+      otherQueue = latestOtherQueue
+      changes.forEach(msg => {
+        console.log('INCOMING', msg)
+        app.ports.incoming.send(msg)
+      })
+    }
   }
 
   app.ports.outgoing.subscribe(function ({ action, payload }) {
-    console.log(action, payload)
+    console.log('OUTGOING', { action, payload })
     switch (action) {
       case 'HOST_GAME':
         return rtc.host({
           id: Date.now(),
-          onData,
-          onConnect: c => { shared.conn = c }
+          onData
         })
-          .then(url => onData({
+          .then(url => app.ports.incoming.send({
             action: 'HOST_URL',
             payload: url
           }))
@@ -78,13 +83,12 @@ const start = () => {
         return rtc.join({
           id: Date.now(),
           serverId: payload,
-          onData,
-          onConnect: c => { shared.conn = c }
+          onData
         })
       case 'SEND_GAME':
-        return rtc.send({
-          conn: shared.conn,
-          msg: { action: 'GAME_RECEIVED', payload }
+        return perform({
+          action: 'GAME_RECEIVED',
+          payload
         })
     }
   })
